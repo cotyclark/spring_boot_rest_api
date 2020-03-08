284fb561ce52f964f06e0d6ba090df15f99643b1
Coty Clark <coty.c@borngroup.com>
2020-03-08 17:38:13 -0400

    First commit

---
+++ src/main/java/com/example/demo/RestRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface RestRepository extends CrudRepository<Beer, Long> {
+
+}
